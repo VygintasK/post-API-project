c97c9fa7aada4e68d7a83702836f2489143036b4
Vygintas <vygintaskirda@gmail.com>
2022-10-26 14:33:06 +0300

    Revoed 2Fech added User html css

--- post.js
+++ post.js
@@ -8,67 +8,46 @@
 
 let allPostsWrapper = document.querySelector('.allPostsWrapper')
 
-fetch('https://jsonplaceholder.typicode.com/posts?_limit=15')
+fetch('https://jsonplaceholder.typicode.com/posts?_limit=15?_commit&_embed=comments&_expand=user')
 .then(response => response.json())
 .then(data => {
     data.forEach(element => {
+
+        let user = element.user
+        let comentsArr = element.comments
         let postWrap = document.createElement('div')
         let postTitle = document.createElement('h2')
-        postTitle.classList.add('postTitle')
+        let authorUser = document.createElement('a')
+        let commentSectionIndicator = document.createElement('h3')
+        let commentWrap = document.createElement('div')
         let postBody = document.createElement('p')
 
-        //iskart apendinu kad nesumaisytu vietu
-        allPostsWrapper.append(postWrap)
+        authorUser.setAttribute('href','#')
+        commentSectionIndicator.classList.add('commentSectionIndicator')
+        postTitle.classList.add('postTitle')
+        commentWrap.classList.add('comment-wraps')
 
-        let postId = element.id
-        let postUserId = element.userId
-
+        authorUser.textContent =`article by: ${user.name}` 
         postTitle.textContent = element.title
         postBody.textContent = element.body
+        commentSectionIndicator.textContent = 'COMMET SECTION:'
         
+        allPostsWrapper.append(postWrap)
+        postWrap.append(postTitle,postBody,authorUser,commentWrap)
+        commentWrap.append (commentSectionIndicator)
 
-        fetch('https://jsonplaceholder.typicode.com/users/'+postUserId)
-        .then(response => response.json())
-        .then(user => {
-            let authorUser = document.createElement('a')
-            authorUser.setAttribute('href','#')
-            
+        comentsArr.forEach(coment => {
+            let commentName = document.createElement('h4')
+            let commentBody = document.createElement('p')
+            let commentEmail = document.createElement('a')
+            commentEmail.setAttribute('href','#')
 
-            authorUser.textContent =`article by: ${user.name}` 
-            
-            // Jei cia appendinsi allPostsWrapper gali sumaisyt
-            // duomenis nes nevienodu laiku paskutiniai duomeny gryzta
-            postWrap.append(postTitle,postBody,authorUser)
+            commentName.textContent = coment.name
+            commentBody.textContent = '- '+coment.body
+            commentEmail.textContent = coment.email
 
-            fetch('https://jsonplaceholder.typicode.com/comments?postId='+postId)
-            .then(response => response.json())
-            .then(comentsArr => {
-
-                let commentSectionIndicator = document.createElement('h3')
-                commentSectionIndicator.classList.add('commentSectionIndicator')
-                let commentWrap = document.createElement('div')
-                commentWrap.classList.add('comment-wraps')
-
-                commentSectionIndicator.textContent = 'COMMET SECTION:'
-                
-                postWrap.append(commentWrap)
-                commentWrap.append (commentSectionIndicator)
-
-
-                comentsArr.forEach(coment => {
-                    let commentName = document.createElement('h4')
-                    let commentBody = document.createElement('p')
-                    let commentEmail = document.createElement('a')
-                    commentEmail.setAttribute('href','#')
-
-                    commentName.textContent = coment.name
-                    commentBody.textContent = '- '+coment.body
-                    commentEmail.textContent = coment.email
-
-                    commentWrap.append(commentName,commentBody,commentEmail)
-                });
-            })
-        })
+            commentWrap.append(commentName,commentBody,commentEmail)
+        });
     })
 })
 // 2. Po kiekvienu įrašu (post) pridėti posto komentarus. Kiekvienas komentaras turi:
