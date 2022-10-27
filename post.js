00810788f64eaad34161916a22e6b51738f01dac
Vygintas <vygintaskirda@gmail.com>
2022-10-27 13:56:23 +0300

    Tiny tweaks

--- post.js
+++ post.js
@@ -7,13 +7,7 @@ fetch(`https://jsonplaceholder.typicode.com/posts?id=${PostID}&_expand=user&_emb
 .then(response => response.json())
 .then(postsArr => {
 
-    // console.log(postsArr[0])
-    // console.log(postsArr[PostID])
-    // console.log(postsArr[PostID].title)
-        
-
     let postWrapper = document.querySelector('.PostWrapper')
-
     let postTitle = document.createElement('h1')
     let postAuthor = document.createElement('a')
     let postAuthorTitle = document.createElement('h4')
@@ -33,18 +27,17 @@ fetch(`https://jsonplaceholder.typicode.com/posts?id=${PostID}&_expand=user&_emb
     postLinkPostsTitle.textContent = "Kiti autoriaus įrašai"
     postLinkPosts.href='./index.html'
 
-
     postLinkPosts.append(postLinkPostsTitle)
     postAuthor.append(postAuthorTitle)
-
 
     let commentTitle = document.createElement('h3')
     let commentWrapUl = document.createElement('ul')
     postComments.append(commentWrapUl)
 
+
+
     commentTitle.textContent = 'ALL COMENTS:'
     comments.forEach(coment => {
-        console.log(coment)
         let commentLi = document.createElement('li')
         let commentName = document.createElement('h4')
         let commentBody = document.createElement('p')
@@ -57,9 +50,7 @@ fetch(`https://jsonplaceholder.typicode.com/posts?id=${PostID}&_expand=user&_emb
         commentName.textContent = coment.name
         commentBody.textContent = coment.body
         commentEmail.textContent = coment.email
-        
 
-        
     });
     postWrapper.append(postTitle, postAuthor, postBody, commentTitle, postComments, postLinkPosts)
 
