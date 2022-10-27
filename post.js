1551575e87fa04d100eaec7b75144df4491c5cb6
Vygintas <vygintaskirda@gmail.com>
2022-10-27 11:02:36 +0300

    Album List added

--- post.js
+++ post.js
@@ -17,14 +17,14 @@ fetch('https://jsonplaceholder.typicode.com/posts?_limit=15?_commit&_embed=comme
         let comentsArr = element.comments
         let postWrap = document.createElement('div')
         let postTitle = document.createElement('h2')
-        let postID = element.id
+        // let postID = element.id
         let authorUser = document.createElement('a')
         let commentSectionIndicator = document.createElement('h3')
         let commentWrap = document.createElement('div')
         let postBody = document.createElement('p')
         
-        postWrap.setAttribute('id',postID)
-        console.log(postID)
+        // postWrap.setAttribute('id',postID)
+        // console.log(postID)
         authorUser.setAttribute('href','./user.html')
         commentSectionIndicator.classList.add('commentSectionIndicator')
         postTitle.classList.add('postTitle')
@@ -52,8 +52,49 @@ fetch('https://jsonplaceholder.typicode.com/posts?_limit=15?_commit&_embed=comme
             commentWrap.append(commentName,commentBody,commentEmail)
         });
     })
+
+
+
+
+
+
+    fetch('https://jsonplaceholder.typicode.com/albums?_limit=15&_expand=user&_embed=photos')
+    .then(response => response.json())
+    .then(albumsArr => {
+
+        let albumWrapElement = document.createElement('div')
+        let albumElementUl = document.createElement('ul')
+        let albumTitleElement = document.createElement('h3')
+        albumTitleElement.textContent = 'ALBUMS LIST:'
+        allPostsWrapper.append(albumWrapElement)
+        albumWrapElement.append(albumTitleElement, albumElementUl)
+
+        albumsArr.forEach(album => {
+            console.log(album.photos[0].thumbnailUrl)  
+
+            let albumElementLi = document.createElement('li')
+            let albumElementName = document.createElement('a')
+            let albumElementAuthor = document.createElement('p')
+            let albumElementPhoto = document.createElement('img')
+            
+            
+            albumElementName.href ='./album.html'
+            albumElementName.textContent = album.title
+            albumElementAuthor.textContent = album.user.name
+            albumElementPhoto.src = album.photos[0].thumbnailUrl
+
+            albumElementLi.innerHTML =`<a href='./album.html'>${album.title}</a>`
+            albumElementLi.append(albumElementName, albumElementAuthor, albumElementPhoto)
+            albumElementUl.append(albumElementLi)
+        });
+    })
 })
 // 2. Po kiekvienu įrašu (post) pridėti posto komentarus. Kiekvienas komentaras turi:
 //   2.1. Komentaro pavadinimą.
 //   2.2. Komentaro turinį - pastraipą.
 //   2.3. Komentarą parašiusio asmens el. pašto adresą.
+
+// 5. Pagrindiniame (index.html) puslapyje pridėti skiltį, kurioje atvaizduojamas albumų sąrašas. Kiekvienas albumas turės:
+//   5.1. Pavadinimą, o paspaudus ant jo - nukreipiama į albumą (album.html).
+//   5.2. Albumo autoriaus vardą.
+//   5.3. Nuotrauką.
--- user.js
+++ user.js
@@ -7,74 +7,84 @@
 //   3.6. Internetinio puslapio adresas.
 //   3.7. Įmonės, kurioje dirba, pavadinimas.
 
-generateUser()
+let userID = 1
 
-function generateUser(){
-    fetch('https://jsonplaceholder.typicode.com/users/1?_embed=posts')
+// `https://jsonplaceholder.typicode.com/users/${userID}/posts`
+fetch(`https://jsonplaceholder.typicode.com/users/${userID}?_embed=posts`)
+.then(response => response.json())
+.then(user => {
+    let usersWrapper = document.querySelector('.usersWrapper')
+
+    let userName = user.name
+    let userNickname = user.username
+    let userEmail = user.email
+    let userPhone = user.phone
+    let userWeb = user.website
+    let userWork = user.company.name
+    let userStreet = user.address.street
+    let userApartment = user.address.suite
+    let userCity = user.address.city
+    let userZipcode = user.address.zipcode
+    
+    let userGeoLat = user.address.geo.lat
+    let userGeoLng = user.address.geo.lng
+    let userGoogle = `https://maps.google.com/maps?q=${userGeoLat},${userGeoLng}`
+
+    let userNameElement = document.createElement('h2')
+    let userNicknameElement = document.createElement('p')
+    let userEmailElement = document.createElement('p')
+    let userPhoneElement = document.createElement('p')
+    let userWebElement = document.createElement('p')
+    let userWorkElement = document.createElement('p')
+    let userAddressElement = document.createElement('p')
+    
+    let userPostUl = document.createElement('ul')
+    let userPostHeader = document.createElement('h3')
+    userPostHeader.textContent = 'All comments:'
+    userPostUl.classList.add('postsWrap')
+
+    let userAlbumUl = document.createElement('ul')
+    let userAlbumTittle = document.createElement('h3')
+    userAlbumTittle.textContent = 'All albums:'
+
+    userNameElement.innerHTML  = userName
+    userNicknameElement.innerHTML  = `<strong>Nickname: </strong>${userNickname}`
+    userEmailElement.innerHTML  = `<strong>E-mail: </strong><a href='mailto:Sincere@april.biz'>${userEmail}</a>`
+    userPhoneElement.innerHTML  = `<strong>Phone: </strong><a href='tel:${userPhone}'>${userPhone}</a>`
+    userWebElement.innerHTML  = `<strong>Web address: </strong><a target='_blank' href='http://www.hildegard.org'>${userWeb}</a>`
+    userWorkElement.innerHTML  = `<strong>Working at: </strong>${userWork}`
+    userAddressElement.innerHTML = `<strong>Address: </strong><a target='_blank' href='${userGoogle}'>${userStreet}, ${userApartment}, ${userCity}, ${userZipcode}</a>`
+
+
+    usersWrapper.append(userNameElement, userNicknameElement, userEmailElement, userPhoneElement, userWebElement, userWorkElement, userAddressElement,userPostHeader,userPostUl, userAlbumTittle, userAlbumUl)
+
+    // 4. Šiame puslapyje turės būti atvaizduojama:
+    //   4.1. Visi vartotojo parašyti įrašai (posts). Post'ų įrašuose nereikia atvaizduoti komentarų. Kiekvienas post'as turi turėti nuorodą.
+    let userPostsArr = user.posts
+    userPostsArr.forEach(post => {
+        let postID = post.id
+        let postTitle = post.title
+        let postTitleElement = document.createElement('li')
+        // console.log(postID)
+        postTitleElement.innerHTML = `post id ${postID}: <a href="./index.html#${postID}">${postTitle}</a>`
+        
+        userPostUl.append(postTitleElement)
+    });
+
+    //   4.2. Visi vartotojo sukurti foto albumai. Kiekvienas albumas turės:
+    //     4.2.1. Albumo pavadinimą, kuris turi būti nuoroda. Kol kas nuoroda gali niekur nevesti.   
+
+    fetch(`https://jsonplaceholder.typicode.com/users/${userID}/albums`)
     .then(response => response.json())
-    .then(user => {
-        let usersWrapper = document.querySelector('.usersWrapper')
+    .then(albumsArr => {
+        albumsArr.forEach(album => {
+            let albumID = album.id
+            let albumTitle = album.title
+            let albumTitleElement = document.createElement('li')
+            albumTitleElement.innerHTML = `album id ${albumID}: <a href='./albums.html'> ${albumTitle}</a>`
 
-        let userName = user.name
-        let userNickname = user.username
-        let userEmail = user.email
-        let userPhone = user.phone
-        let userWeb = user.website
-        let userWork = user.company.name
-        let userStreet = user.address.street
-        let userApartment = user.address.suite
-        let userCity = user.address.city
-        let userZipcode = user.address.zipcode
-        
-        let userGeoLat = user.address.geo.lat
-        let userGeoLng = user.address.geo.lng
-        let userGoogle = `https://maps.google.com/maps?q=${userGeoLat},${userGeoLng}`
+            userAlbumUl.append(albumTitleElement) 
 
-        let userNameElement = document.createElement('h2')
-        let userNicknameElement = document.createElement('p')
-        let userEmailElement = document.createElement('p')
-        let userPhoneElement = document.createElement('p')
-        let userWebElement = document.createElement('p')
-        let userWorkElement = document.createElement('p')
-        let userAddressElement = document.createElement('p')
-        
-        let userPostWrap = document.createElement('ul')
-        let userPostHeader = document.createElement('h3')
-        userPostHeader.textContent = 'All comments:'
-        userPostWrap.classList.add('postsWrap')
-
-        userNameElement.innerHTML  = userName
-        userNicknameElement.innerHTML  = `<strong>Nickname: </strong>${userNickname}`
-        userEmailElement.innerHTML  = `<strong>E-mail: </strong><a href='mailto:Sincere@april.biz'>${userEmail}</a>`
-        userPhoneElement.innerHTML  = `<strong>Phone: </strong><a href='tel:${userPhone}'>${userPhone}</a>`
-        userWebElement.innerHTML  = `<strong>Web address: </strong><a target='_blank' href='http://www.hildegard.org'>${userWeb}</a>`
-        userWorkElement.innerHTML  = `<strong>Working at: </strong>${userWork}`
-        userAddressElement.innerHTML = `<strong>Address: </strong><a target='_blank' href='${userGoogle}'>${userStreet}, ${userApartment}, ${userCity}, ${userZipcode}</a>`
-
-
-        usersWrapper.append(userNameElement, userNicknameElement, userEmailElement, userPhoneElement, userWebElement, userWorkElement, userAddressElement,userPostHeader,userPostWrap)
-        userPostWrap.append()
-
-        // 4. Šiame puslapyje turės būti atvaizduojama:
-        //   4.1. Visi vartotojo parašyti įrašai (posts). Post'ų įrašuose nereikia atvaizduoti komentarų. Kiekvienas post'as turi turėti nuorodą.
-        let userPostsArr = user.posts
-        userPostsArr.forEach(post => {
-            let postID = post.id
-            let postTitle = post.title
-            let postTitleElement = document.createElement('li')
-            console.log(postID)
-            postTitleElement.innerHTML = `post id${postID}: <a href="./index.html#${postID}">${postTitle}</a>`
-            
-            userPostWrap.append(postTitleElement)
-        });
-
-        //   4.2. Visi vartotojo sukurti foto albumai. Kiekvienas albumas turės:
-        //     4.2.1. Albumo pavadinimą, kuris turi būti nuoroda. Kol kas nuoroda gali niekur nevesti.   
-
-
-
-
-
+        })
     })
-}
-
+})
