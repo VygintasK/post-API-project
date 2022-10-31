3d322f42b0b53308f2d37dcf6448a0ec34f05b3e
Vygintas <vygintaskirda@gmail.com>
2022-10-31 16:10:49 +0200

    SEARCH made 3 category,   (fixed grammar)

--- JS/search.js
+++ JS/search.js
@@ -5,72 +5,28 @@ async function init(){
     console.log(queryParams)
     console.log(searchInput)
     
-    let categoryArray = ['users', 'albums', 'posts']
-    let searchWrapper = document.querySelector('.searchWraper')
-    let inccorectSearchValue = document.createElement('h4')
+    // let categoryArray = ['users', 'albums', 'posts']
+    let searchWrapper = document.querySelector('.searchWrapper')
+    let incorrectSearchValue = document.createElement('h4')
     
     if (!searchInput) {
-        inccorectSearchValue.textContent = 'Write something dimwit.'
-        searchWrapper.append(inccorectSearchValue)
+        incorrectSearchValue.textContent = 'Write something dimwit.'
+        searchWrapper.append(incorrectSearchValue)
     }
     else if (!Number(searchInput) === false){
-        inccorectSearchValue.textContent = 'No numbers dimwit.'
-        searchWrapper.append(inccorectSearchValue)
+        incorrectSearchValue.textContent = 'No numbers dimwit.'
+        searchWrapper.append(incorrectSearchValue)
     } else 
     {
 
+        let users = 'users'
+        let albums = 'albums'
+        let posts = 'posts'
 
 
-
-
-
-        
-        categoryArray.forEach(category => {
-            let infoElement = document.createElement('h2')
-            let foundWrapper = document.createElement('div')
-            foundWrapper.classList.add('foundWrapper')
-
-            fetch(`https://jsonplaceholder.typicode.com/${category}?q=${searchInput}`)
-            .then(response => response.json())
-            .then(result => {
-                
-                if(result.length >= 1){
-                    
-                    infoElement.textContent =`Found ( ${result.length} ) results in "${category}" category`
-                    
-                    result.forEach(element => {
-                        let foundResult = document.createElement('p')
-                        foundResult.style.display = "block"
-                        
-                        let {title,name, id} = element                                         
-    
-                        if(category === 'posts'){
-                            foundResult.innerHTML=`<a href='post.html?post_id=${id}'>-- ${title}</a>`
-                        }
-                        else if(category === 'albums'){
-                            foundResult.innerHTML=`<a href='album.html?album_id=${id}'>++ ${title}</a>`  
-                        }
-                        else if(category === 'users'){
-                            foundResult.innerHTML=`<a href='user.html?user_id=${id}'>- ${name}</a>`
-                        } else {
-                            foundResult.textContent= `some sort of ERROR, what do i know`
-                        }
-                        foundWrapper.append(foundResult)
-                    });
-        
-                } else{
-                    infoElement.textContent = `Sorry, nothing found in "${category}" category`
-                }
-                searchWrapper.append(infoElement, foundWrapper)
-            }) 
-        });
-
-
-
-        
-
-
-
+        fetchingCategory(users, searchInput, searchWrapper)
+        fetchingCategory(albums, searchInput, searchWrapper)
+        fetchingCategory(posts, searchInput, searchWrapper)
 
         //////////////////////BACKUP BE ASYNC/////////////////////////////////
         // categoryArray.forEach(category => {
@@ -119,5 +75,41 @@ init()
 
 
 
+function fetchingCategory(category, searchInput,searchWrapper ){
+    let infoElement = document.createElement('h2')
+    let foundWrapper = document.createElement('div')
+    foundWrapper.classList.add('foundWrapper')
 
+    fetch(`https://jsonplaceholder.typicode.com/${category}?q=${searchInput}`)
+    .then(response => response.json())
+    .then(result => {              
+        if(result.length >= 1){
+            infoElement.textContent =`Found ( ${result.length} ) results in "${category}" category`
+            
+            result.forEach(element => {
+                let foundResult = document.createElement('p')
+                foundResult.style.display = "block"
+                
+                let {title,name, id} = element                                         
+
+                if(category === 'posts'){
+                    foundResult.innerHTML=`<a href='post.html?post_id=${id}'>-- ${title}</a>`
+                }
+                else if(category === 'albums'){
+                    foundResult.innerHTML=`<a href='album.html?album_id=${id}'>++ ${title}</a>`  
+                }
+                else if(category === 'users'){
+                    foundResult.innerHTML=`<a href='user.html?user_id=${id}'>- ${name}</a>`
+                } else {
+                    foundResult.textContent= `some sort of ERROR, what do i know`
+                }
+                foundWrapper.append(foundResult)
+            });
+
+        } else{
+            infoElement.textContent = `Sorry, nothing found in "${category}" category`
+        }
+        searchWrapper.append(infoElement, foundWrapper)
+    }) 
+} 
 
