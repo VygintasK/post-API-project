cfef824dc3a5bea5cfa432294668de20f84d8adb
Vygintas <vygintaskirda@gmail.com>
2022-11-03 18:18:17 +0200

    Finally Search works - PARAMETRAI nera KINTAMIEJI

--- JS/search.js
+++ JS/search.js
@@ -1,44 +1,66 @@
-init()
+import initHeader from './header.js'
+import {param} from './functions.js'
 
-async function init(){
-    const queryParams = document.location.search;
-    const urlParams = new URLSearchParams(queryParams);
-    const searchInput = urlParams.get('form_name_search');
-    console.log(queryParams)
-    console.log(searchInput)
+initHeader()
+initSearch()
+
+async function initSearch(){
+    const searchInput = param('form_name_search')
     
     let searchWrapper = document.querySelector('.searchWrapper')
     let searchResultWrap = document.querySelector('.searchResultWrap')
-    console.log(searchResultWrap)
     
-    validAndSearch(searchInput, searchWrapper)
-
-    searchLocal(searchWrapper)
-
-
+    validAndSearch(searchInput,searchResultWrap )
+    searchLocal(searchWrapper, searchResultWrap)
 }
 
-async function validAndSearch(searchInput, searchWrapper ){
+async function searchLocal(searchWrapper, searchResultWrap){
+    let searchFormLocal = document.createElement('form')
+    searchFormLocal.id = 'searchFormLocal'
+    let InputTextElementLocal = document.createElement('input')
+    InputTextElementLocal.id = 'searchTextLocal'
+    InputTextElementLocal.name = 'form_name_search_local'
+    InputTextElementLocal.type = 'text'
+    let submitLocal = document.createElement('input')
+    submitLocal.type='submit'
+    submitLocal.value = 'FIND'
+    
+    searchWrapper.prepend(searchFormLocal)
+    searchFormLocal.append(InputTextElementLocal,submitLocal)
+    
+    searchFormLocal.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let inputText = event.target.elements.searchTextLocal.value
+
+        validAndSearch(inputText, searchResultWrap)
+        searchFormLocal.reset()
+    })
+}
+
+async function validAndSearch(searchInput, searchResultWrap){
     let incorrectSearchValue = document.createElement('h4')
+    searchResultWrap.innerHTML = ''
 
     if (!searchInput) {
         incorrectSearchValue.textContent = 'Write something dimwit.'
-        searchWrapper.append(incorrectSearchValue)
+        searchResultWrap.append(incorrectSearchValue)
     }
     else if (!Number(searchInput) === false){
         incorrectSearchValue.textContent = 'No numbers dimwit.'
-        searchWrapper.append(incorrectSearchValue)
+        searchResultWrap.append(incorrectSearchValue)
     } else 
     {
-        await fetchCategory('users', searchInput, searchWrapper)
-        await fetchCategory('albums', searchInput, searchWrapper)
-        await fetchCategory('posts', searchInput, searchWrapper)
+        await fetchCategory('users', searchInput, searchResultWrap)
+        await fetchCategory('albums', searchInput, searchResultWrap)
+        await fetchCategory('posts', searchInput, searchResultWrap)
     }
+
 }
-async function fetchCategory(category, searchInput,searchWrapper ){
+
+async function fetchCategory(category, searchInput, searchResultWrap ){
     let infoElement = document.createElement('h2')
-    let foundWrapper = document.createElement('ul')
-    foundWrapper.classList.add('foundWrapper')
+    let foundUL = document.createElement('ul')
+    foundUL.classList.add('foundUL')
 
     const response = await fetch(`https://jsonplaceholder.typicode.com/${category}?q=${searchInput}`)
     const result = await response.json()
@@ -51,48 +73,27 @@ async function fetchCategory(category, searchInput,searchWrapper ){
             foundResult.style.display = "block"
             
             let {title,name, id} = element                                         
-            if(category === 'posts'){
+            if(category === 'users'){
+                foundResult.innerHTML=`<a href='user.html?user_id=${id}'>- ${name}</a>`
+            } 
+            else if(category === 'posts'){
                 foundResult.innerHTML=`<a href='post.html?post_id=${id}'>-- ${title}</a>`
             }
             else if(category === 'albums'){
                 foundResult.innerHTML=`<a href='album.html?album_id=${id}'>++ ${title}</a>`  
             }
-            else if(category === 'users'){
-                foundResult.innerHTML=`<a href='user.html?user_id=${id}'>- ${name}</a>`
-            } else {
+            else {
                 foundResult.textContent= `some sort of ERROR, what do i know`
             }
-            foundWrapper.append(foundResult)
+            foundUL.append(foundResult)
         });
 
     } else{
         infoElement.textContent = `Sorry, nothing found in "${category}" category`
     }
-    searchWrapper.append(infoElement, foundWrapper)
+    searchResultWrap.append(infoElement, foundUL)
 
 } 
-async function searchLocal(searchWrapper){
-    let searchFormLocal = document.createElement('form')
-    searchFormLocal.id = 'searchFormLocal'
-    let InputTextElementLocal = document.createElement('input')
-    InputTextElementLocal.id = 'searchTextLocal'
-    InputTextElementLocal.name = 'form_name_search_local'
-    InputTextElementLocal.type = 'text'
-    let submitLocal = document.createElement('input')
-    submitLocal.type='submit'
-    submitLocal.value = 'FIND'
-    
 
-    searchWrapper.prepend(searchFormLocal)
-    searchFormLocal.append(InputTextElementLocal,submitLocal)
-    
 
-    searchFormLocal.addEventListener('submit', (event) => {
-        event.preventDefault();
-        let inputText = event.target.elements.searchTextLocal.value
-
-        validAndSearch(inputText, searchWrapper)
-        searchFormLocal.reset()
-    })
-}
     // 11.5. Search puslapyje turi būti paieškos forma, kuri veikia neperkraunant puslapio.
